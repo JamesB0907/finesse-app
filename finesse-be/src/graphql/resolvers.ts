402fbab4adc72ad16917a33c6d3ea58ee758d436
James Brooks <virverum22@gmail.com>
2024-03-03 15:39:18 -0500

    Added quarterly and annual query

--- finesse-be/src/graphql/resolvers.ts
+++ finesse-be/src/graphql/resolvers.ts
@@ -1,29 +1,48 @@
 import { Client } from '@apperate/iexjs';
-import { Resolver } from 'dns';
-import GraphQLJSON from "graphql-type-json";
-import { Resolvers } from '../generated/graphql';
+import { BigIntResolver, DateResolver, JSONResolver } from 'graphql-scalars';
+import { GqlResolvers } from '../generated/graphql';
 
 const client = new Client({
-    api_token: process.env.IEX_API_TOKEN,
-    version: 'v1',
-  });
-  
-  const getQuote = (symbol: string) => {
-    return client.quote({ symbol: symbol });
-  };
-  
+  api_token: process.env.IEX_API_TOKEN,
+  version: 'v1',
+});
 
-const resolvers: Resolvers = {
-    JSON: GraphQLJSON,
-    Query: {
-      hello: () => 'world',
-    },
-    
-    Mutation: {
-      quote: (_: any, { symbol }) => {
-        return getQuote(symbol);
-      },
-    },
-  };
+const getQuote = (symbol: string) => {
+  return client.quote({ symbol: symbol });
+};
 
-  export default resolvers;
+const resolvers: GqlResolvers = {
+  JSON: JSONResolver,
+  Date: DateResolver,
+  BigInt: BigIntResolver,
+
+  Query: {
+    lookup: (_, { symbol }) => {
+      return { symbol };
+    },
+  },
+
+  Mutation: {
+    quote: (_: any, { symbol }) => {
+      return getQuote(symbol);
+    },
+  },
+
+  Lookup: {
+    revenue: async (lookup, { resolution }) => {
+      const result = await client.timeSeries({
+        id: 'INCOME',
+        key: lookup.symbol,
+        limit: 100,
+        range: '5y',
+        subkey: resolution,
+      });
+      return result.map((point) => ({
+        date: point.fiscalDate,
+        value: point.totalRevenue,
+      }));
+    },
+  },
+};
+
+export default resolvers;
--- finesse-be/typings.d.ts
+++ finesse-be/typings.d.ts
@@ -1,1 +1,55 @@
-declare module "@apperate/iexjs";
+declare module "@apperate/iexjs" {
+type ClientOptions = {
+    api_token?: string;
+    version: "v1";
+    };
+
+    type Dataset = "INCOME";
+
+    type TimeSeriesOptions = {
+        key: string;
+        id: Dataset;
+        subkey?: string;
+        range?: string;
+        limit?: number;
+    };
+
+    type FilingType = "10-K";
+
+    type IncomeDataPoint = {
+        costOfRevenue: number;
+        currency: number;
+        ebit: 16118000000;
+        filingType: FilingType;
+        fiscalDate: string;
+        fiscalQuarter: number;
+        fiscalYear: number;
+        grossProfit: number;
+        incomeTax: number;
+        interestIncome: number;
+        netIncome: number;
+        netIncomeBasic: number;
+        operatingExpense: number;
+        operatingIncome: number;
+        otherIncomeExpenseNet: number;
+        pretaxIncome: number;
+        reportDate: string;
+        researchAndDevelopment: number;
+        sellingGeneralAndAdmin: number;
+        symbol: string;
+        totalRevenue: number;
+        id: "INCOME";
+        date: number;
+        updated: number;
+    };
+
+    class Client {
+        constructor(options: ClientOptions);
+
+        quote(options: { symbol: string });
+
+        timeSeries(
+            options: TimeSeriesOptions & { id: "INCOME" }
+        ): Promise<IncomeDataPoint[]>;
+    }
+}
